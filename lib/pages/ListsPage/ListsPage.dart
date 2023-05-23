0f97323ffa9b601f2f3586be92064b7b54a6a098
YasinSefa15 <yasekirman@gmail.com>
2023-05-24 01:29:24 +0300

    list page draft

--- lib/pages/ListsPage/ListsPage.dart
+++ lib/pages/ListsPage/ListsPage.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:pain4gain/components/CustomBottomNavigationBar.dart';
+import 'package:pain4gain/components/lists/list_option.dart';
 
-class ListsPage extends StatelessWidget {
-  const ListsPage({super.key});
+import '../../components/lists/list_categories.dart';
+
+class ListsPage extends StatefulWidget {
+  @override
+  _ListsPageState createState() => _ListsPageState();
+}
+
+class _ListsPageState extends State<ListsPage> {
+  bool isOption = false;
+
+  void changeOption(bool value) {
+    setState(() {
+      isOption = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Merhaba',
-          style: TextStyle(fontSize: 24),
+    return Column(
+      children: [
+        ListOption(
+          onOptionChanged: (value) {
+            changeOption(value);
+          },
         ),
-      ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(isOption.toString()),
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: ListCategories(),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(80, (index) {
+              return Container(
+                margin: const EdgeInsets.only(right: 5),
+                width: 30,
+                height: 30,
+                color: Colors.blue,
+                child: const Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              );
+            }),
+          ),
+        )
+      ],
     );
   }
 }
