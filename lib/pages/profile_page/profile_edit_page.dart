69a7f7c79ac12a174c383f85f1cbc2efd1912040
AlperOzdmir <aalper.ozdemirr@gmail.com>
2023-05-25 00:39:35 +0300

    Added - profile_edit_page.dart with proper pubspec files adjustments.

---
+++ lib/pages/profile_page/profile_edit_page.dart
@@ -0,0 +1,368 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfileEditingPage extends StatefulWidget {
+  const ProfileEditingPage({Key? key}) : super(key: key);
+
+  @override
+  ProfileEditingPageState createState() => ProfileEditingPageState();
+}
+
+class ProfileEditingPageState extends State<ProfileEditingPage> {
+  late TextEditingController _nameController;
+  late TextEditingController _ageController;
+  late TextEditingController _heightController;
+  late TextEditingController _weightController;
+  late TextEditingController _workoutDaysController;
+  String _gender = '';
+
+  Map<String, String> _errors = {
+    'name': '',
+    'age': '',
+    'height': '',
+    'weight': '',
+    'workoutDays': '',
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _ageController = TextEditingController();
+    _heightController = TextEditingController();
+    _weightController = TextEditingController();
+    _workoutDaysController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    _heightController.dispose();
+    _weightController.dispose();
+    _workoutDaysController.dispose();
+    super.dispose();
+  }
+
+  void _saveProfile() {
+    // Reset error messages
+    setState(() {
+      _errors = {};
+    });
+
+    // Validate the input
+    if (_nameController.text.isEmpty) {
+      setState(() {
+        _errors['name'] = 'Please enter your name';
+      });
+    }
+
+    int? age = int.tryParse(_ageController.text);
+    if (age == null || age <= 6 || age >= 101) {
+      setState(() {
+        _errors['age'] = 'Please enter a valid age';
+      });
+    }
+
+    double? height = double.tryParse(_heightController.text);
+    if (height == null || height <= 69 || height >= 251) {
+      setState(() {
+        _errors['height'] = 'Please enter a valid height';
+      });
+    }
+
+    double? weight = double.tryParse(_weightController.text);
+    if (weight == null || weight <= 29 || weight >= 301) {
+      setState(() {
+        _errors['weight'] = 'Please enter a valid weight';
+      });
+    }
+
+    int? workoutDays = int.tryParse(_workoutDaysController.text);
+    if (workoutDays == null || workoutDays < 1 || workoutDays > 7) {
+      setState(() {
+        _errors['workoutDays'] = 'Please enter a valid number of workout days (1-7)';
+      });
+    }
+
+    if (_gender.isEmpty) {
+      setState(() {
+        _errors['gender'] = 'Please select a gender';
+      });
+    }
+
+    if (_errors.isNotEmpty) {
+      return;
+    }
+
+    // Save the profile data using shared_preferences
+    _saveProfileData().then((_) {
+      const snackBar = SnackBar(
+        content: Text('Profile data changed successfully!'),
+        duration: Duration(seconds: 2),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      // Navigate back to the previous screen
+      Future.delayed(const Duration(seconds: 2), () {
+        Navigator.pop(context);
+      });
+    });
+  }
+
+  Future<void> _saveProfileData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('name', _nameController.text);
+    await prefs.setInt('age', int.parse(_ageController.text));
+    await prefs.setDouble('height', double.parse(_heightController.text));
+    await prefs.setDouble('weight', double.parse(_weightController.text));
+    await prefs.setString('gender', _gender);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Name',
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.person),
+                      labelText: 'Enter your name',
+                      errorText: _errors['name'],
+                      labelStyle: const TextStyle(color: Colors.black),
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      errorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedErrorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Age',
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  TextField(
+                    controller: _ageController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.calendar_today),
+                      labelText: 'Enter your age',
+                      errorText: _errors['age'],
+                      labelStyle: const TextStyle(color: Colors.black),
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      errorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedErrorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Height',
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  TextField(
+                    controller: _heightController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.height),
+                      labelText: 'Enter your height (cm)',
+                      errorText: _errors['height'],
+                      labelStyle: const TextStyle(color: Colors.black),
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      errorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedErrorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Weight',
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  TextField(
+                    controller: _weightController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.fitness_center),
+                      labelText: 'Enter your weight (kg)',
+                      errorText: _errors['weight'],
+                      labelStyle: const TextStyle(color: Colors.black),
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      errorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedErrorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Workout Days',
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  TextField(
+                    controller: _workoutDaysController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.calendar_today),
+                      labelText: 'Enter number of workout days (1-7)',
+                      errorText: _errors['workoutDays'],
+                      labelStyle: const TextStyle(color: Colors.black),
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      errorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                      focusedErrorBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black45),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Gender',
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Radio<String>(
+                        value: 'male',
+                        groupValue: _gender,
+                        onChanged: (value) {
+                          setState(() {
+                            _gender = value!;
+                          });
+                        },
+                      ),
+                      const Text('Male'),
+                      Radio<String>(
+                        value: 'female',
+                        groupValue: _gender,
+                        onChanged: (value) {
+                          setState(() {
+                            _gender = value!;
+                          });
+                        },
+                      ),
+                      const Text('Female'),
+                    ],
+                  ),
+                  if (_errors.containsKey('gender'))
+                    Text(
+                      _errors['gender']!,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: _saveProfile,
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ProfileEditingPage(),
+  ));
+}
