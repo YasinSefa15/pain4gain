4b07343675d4811a099560b3f3938e1c0a913541
Semihhasal <semih.hasal@htomail.com>
2023-05-25 14:30:29 +0300

    meals for today pages

--- lib/pages/diet_page/DietPage.dart
+++ lib/pages/diet_page/DietPage.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'meal.dart';
+
 class DietPage extends StatelessWidget {
   const DietPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-   return Scaffold(
-  backgroundColor: const Color(0xFFE9E9E9),
-  
- 
+    return Scaffold(
+        backgroundColor: const Color(0xFFE9E9E9),
         body: Stack(
           children: <Widget>[
             Positioned(
@@ -27,15 +27,103 @@ class DietPage extends StatelessWidget {
               ),
             ),
             Positioned(
-              top: height*0.38,
+              top: height * 0.38,
               left: 0,
               right: 0,
               child: Container(
-                color: Colors.grey,
-                height: height*0.5,
+                height: height * 0.6,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 8,
+                        left: 32,
+                        right: 16,
+                      ),
+                      child: Text(
+                        "MEALS FOR TODAY",
+                        style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width: 32,
+                            ),
+                            for (int i = 0; i < meals.length; i++)
+                              _MealCard(
+                                meal: meals[i],
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Expanded(
+                      child: Container(
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
         ));
+  }
+}
+
+class _MealCard extends StatelessWidget {
+  final Meal meal;
+
+  const _MealCard({Key? key, required this.meal}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        right: 20,
+        bottom: 10,
+      ),
+      child: Material(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        elevation: 4,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Flexible(
+              fit: FlexFit.tight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                child: Image.asset(
+                  meal.imagePath,
+                  width: 150,
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Column(
+                children: [
+                  Text(meal.mealTime),
+                  Text(meal.name),
+                  Text(meal.kiloCaloriesBurnt),
+                  Text(meal.timeTaken),
+                  SizedBox(height: 16),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
