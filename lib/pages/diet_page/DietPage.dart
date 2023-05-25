a3d602684e524964412ed098cb19de098d3c3d94
Semihhasal <semih.hasal@htomail.com>
2023-05-25 13:03:00 +0300

    dietsayfası ikiye bolme

--- lib/pages/diet_page/DietPage.dart
+++ lib/pages/diet_page/DietPage.dart
@@ -5,14 +5,37 @@ class DietPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Merhaba, Burası Diet Sayfası.',
-          //deneme
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
+    final height = MediaQuery.of(context).size.height;
+   return Scaffold(
+  backgroundColor: const Color(0xFFE9E9E9),
+  
+ 
+        body: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              height: height * 0.35,
+              left: 0,
+              right: 0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(40),
+                ),
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Positioned(
+              top: height*0.38,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.grey,
+                height: height*0.5,
+              ),
+            ),
+          ],
+        ));
   }
 }
