749469589166afdbb1674d5ec9fbd68f20902365
YasinSefa15 <yasekirman@gmail.com>
2023-05-25 22:41:44 +0300

    inside the list draft

--- lib/pages/ListsPage/workout_page.dart
+++ lib/pages/ListsPage/workout_page.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class WorkoutPage extends StatelessWidget {
   final String workoutTitle;
   final String workoutColor;
+  final List<dynamic> workoutList;
 
   const WorkoutPage(
-      {super.key, required this.workoutTitle, required this.workoutColor});
+      {super.key,
+      required this.workoutTitle,
+      required this.workoutColor,
+      required this.workoutList});
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +18,23 @@ class WorkoutPage extends StatelessWidget {
         title: Text(workoutTitle),
         backgroundColor: Color(int.parse(workoutColor)),
       ),
-      body: Container(
-          // İkinci sayfanın içeriği
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: workoutList.length,
+              itemBuilder: (context, index) {
+                print(workoutList[index]);
+                return ListTile(
+                  title: Text(workoutList[index]['name']),
+                  subtitle: Text("asdsd"),
+                  leading: Image.network(workoutList[index]['gif']),
+                );
+              },
+            ),
           ),
+        ],
+      ),
     );
   }
 }
