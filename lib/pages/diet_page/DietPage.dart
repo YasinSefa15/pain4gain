7b6f6c6439e8342f1cc810bc2e1d11e7ccc3960a
Emirhan Erdoğan <emrhnerdogan01@gmail.com>
2023-05-31 18:28:45 +0300

    -Diet page Menu bar made operational

--- lib/pages/diet_page/DietPage.dart
+++ lib/pages/diet_page/DietPage.dart
@@ -3,7 +3,7 @@ import 'package:pain4gain/pages/diet_page/meal_detail_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vector_math/vector_math_64.dart' as mathe;
 import 'package:intl/intl.dart';
-import 'meal.dart';
+import 'package:pain4gain/pages/diet_page/meal.dart';
 import 'package:animations/animations.dart';
 
 class DietPage extends StatefulWidget {
@@ -18,6 +18,27 @@ class DietPageState extends State<DietPage> with SingleTickerProviderStateMixin{
     late TabController _tabController;
     final selectedColor = Color(0xff1a73e8);
     final unselectedColor = Color(0xff5f6368);
+
+  final List<Meal> menu1Meals = [
+    Meal(name: 'Meal 1', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+    Meal(name: 'Meal 2', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+    Meal(name: 'Meal 3', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+  ];
+
+  final List<Meal> menu2Meals = [
+    Meal(name: 'Meal 1', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+    Meal(name: 'Meal 2', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+    Meal(name: 'Meal 3', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+
+  ];
+
+  final List<Meal> menu3Meals = [
+    Meal(name: 'Meal 1', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+    Meal(name: 'Meal 2', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+    Meal(name: 'Meal 3', mealTime: '', imagePath: '', kiloCaloriesBurnt: '', timeTaken: '', preparation: '', ingredients: []),
+
+  ];
+
     final tabs = [
       Tab(text: 'MENU 1'),
       Tab(text: 'MENU 2'),
@@ -26,7 +47,7 @@ class DietPageState extends State<DietPage> with SingleTickerProviderStateMixin{
 
  @override
   void initState() {
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: tabs.length, vsync: this);
     super.initState();
   }
 
@@ -154,9 +175,16 @@ class DietPageState extends State<DietPage> with SingleTickerProviderStateMixin{
                 labelColor: Colors.white,
                 unselectedLabelColor: Colors.black,
                 tabs: tabs,
+                onTap: (index) {
+                  setState(() {
+                    _tabController.index = index;
+                  });
+                },
               ),
+
             ),
             ),
+
             Positioned(
               top: height * 0.43,
               left: 0,
@@ -181,19 +209,40 @@ class DietPageState extends State<DietPage> with SingleTickerProviderStateMixin{
                       ),
                     ),
                     Expanded(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: <Widget>[
-                            SizedBox(
-                              width: 32,
+                      child: TabBarView(
+                        controller: _tabController,
+                        children: [
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(width: 32),
+                                for (int i = 0; i < menu1Meals.length; i++)
+                                  _MealCard(meal: menu1Meals[i]),
+                              ],
                             ),
-                            for (int i = 0; i < meals.length; i++)
-                              _MealCard(
-                                meal: underweight_MENU1[i],// yemeklerin konulması gereken yer hesaplanıp hangisi ise buraya geçirilecek o günün menüsü
-                              ),
-                          ],
-                        ),
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(width: 32),
+                                for (int i = 0; i < menu2Meals.length; i++)
+                                  _MealCard(meal: menu2Meals[i]),
+                              ],
+                            ),
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(width: 32),
+                                for (int i = 0; i < menu3Meals.length; i++)
+                                  _MealCard(meal: menu3Meals[i]),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                     SizedBox(height: 20),
@@ -217,145 +266,6 @@ class DietPageState extends State<DietPage> with SingleTickerProviderStateMixin{
     username = storedUsername!;
   }
 }
-
-
-class _IngredientProgress extends StatelessWidget {
-  final String ingredient;
-  final int leftAmount;
-  final double progress, width;
-  final Color progressColor;
-
-  const _IngredientProgress(
-      {Key? key,
-      required this.ingredient,
-      required this.leftAmount,
-      required this.progress,
-      required this.progressColor,
-      required this.width})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          ingredient.toUpperCase(),
-          style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                Container(
-                  height: 10,
-                  width: width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                    color: Colors.black12,
-                  ),
-                ),
-                Container(
-                  height: 10,
-                  width: width * progress,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                    color: progressColor,
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text("${leftAmount}g left"),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-class _RadialProgress extends StatelessWidget {
-  const _RadialProgress(
-      {super.key,
-      required this.height,
-      required this.width,
-      required this.progress});
-  final double height, width, progress;
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: _RadialPainter(
-        progress: 0.7,
-      ),
-      child: Container(
-        height: height,
-        width: width,
-        child: Center(
-          child: RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: "1731", // kalan ihriyac yemeğin değeri girilecek
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w700,
-                    color: const Color(0xFF200087),
-                  ),
-                ),
-                TextSpan(text: "\n"),
-                TextSpan(
-                  text: "kcal left",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: const Color(0xFF200087),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _RadialPainter extends CustomPainter {
-  final double progress;
-
-  _RadialPainter({required this.progress});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..strokeWidth = 10
-      ..color = Color(0xFF200087)
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    Offset center = Offset(size.width / 2, size.height / 2);
-    double relativeProgress = 360 * progress;
-    canvas.drawArc(Rect.fromCircle(center: center, radius: size.width / 2),
-        mathe.radians(-90), mathe.radians(-relativeProgress), false, paint);
-    // grafiğin dönmesi vs yapılacak
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    //TODO: implement shouldRepaint
-    return true;
-  }
-}
-
 class _MealCard extends StatelessWidget {
   final Meal meal;
 
@@ -465,3 +375,250 @@ class _MealCard extends StatelessWidget {
     );
   }
 }
+
+class _IngredientProgress extends StatelessWidget {
+  final String ingredient;
+  final int leftAmount;
+  final double progress, width;
+  final Color progressColor;
+
+  const _IngredientProgress(
+      {Key? key,
+      required this.ingredient,
+      required this.leftAmount,
+      required this.progress,
+      required this.progressColor,
+      required this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          ingredient.toUpperCase(),
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: 10,
+                  width: width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: Colors.black12,
+                  ),
+                ),
+                Container(
+                  height: 10,
+                  width: width * progress,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: progressColor,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text("${leftAmount}g left"),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _RadialProgress extends StatelessWidget {
+  const _RadialProgress(
+      {
+      required this.height,
+      required this.width,
+      required this.progress});
+  final double height, width, progress;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _RadialPainter(
+        progress: 0.7,
+      ),
+      child: Container(
+        height: height,
+        width: width,
+        child: Center(
+          child: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "1731", // kalan ihriyac yemeğin değeri girilecek
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    color: const Color(0xFF200087),
+                  ),
+                ),
+                TextSpan(text: "\n"),
+                TextSpan(
+                  text: "kcal left",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xFF200087),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RadialPainter extends CustomPainter {
+  final double progress;
+
+  _RadialPainter({required this.progress});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..strokeWidth = 10
+      ..color = Color(0xFF200087)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double relativeProgress = 360 * progress;
+    canvas.drawArc(Rect.fromCircle(center: center, radius: size.width / 2),
+        mathe.radians(-90), mathe.radians(-relativeProgress), false, paint);
+    // grafiğin dönmesi vs yapılacak
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    //TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+/*class _MealCard extends StatelessWidget {
+  final Meal meal;
+
+  const _MealCard({Key? key, required this.meal}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        right: 20,
+        bottom: 10,
+      ),
+      child: Material(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Flexible(
+              fit: FlexFit.tight,
+              child: OpenContainer(
+                closedShape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                transitionDuration: const Duration(milliseconds: 1000),
+                openBuilder: (context, _) {
+                  return MealDetailScreen(
+                    meal: meal,
+                  );
+                },
+                closedBuilder: (context, openContainer) {
+                  return GestureDetector(
+                    onTap: openContainer,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      child: Image.asset(
+                        meal.imagePath,
+                        width: 150,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(height: 5),
+                    Text(
+                      meal.mealTime,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Text(
+                      meal.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "${meal.kiloCaloriesBurnt} kcal",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.access_time,
+                          size: 15,
+                          color: Colors.black12,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          "${meal.timeTaken} min",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}*/
