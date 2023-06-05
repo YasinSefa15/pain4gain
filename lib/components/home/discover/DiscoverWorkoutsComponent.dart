8c20aa6c662023458df493ac0dd327b0450ebcd3
erennbaspinar <ishakeren2001@gmail.com>
2023-06-06 00:30:04 +0300

    Error fix

--- lib/components/home/discover/DiscoverWorkoutsComponent.dart
+++ lib/components/home/discover/DiscoverWorkoutsComponent.dart
@@ -16,120 +16,111 @@ class DiscoverWorkoutsComponent extends StatelessWidget {
         maxHeight: constraints.maxHeight,
       ),
       padding: EdgeInsets.only(left: 20, right: 20),
-      child: Expanded(
-        child: SizedBox(
-          height: screenHeight / 3.5,
-          width: screenWidth,
-          child: DecoratedBox(
+      child: SizedBox(
+        height: screenHeight / 3.5,
+        width: screenWidth,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  Color.fromRGBO(196, 231, 89, 100),
+                  Color.fromRGBO(109, 225, 149, 100)
+                ],
+              ),
+              borderRadius: BorderRadius.circular(10)),
+          child: Container(
             decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
                 gradient: LinearGradient(
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.topRight,
-                  colors: [
-                    Color.fromRGBO(196, 231, 89, 100),
-                    Color.fromRGBO(109, 225, 149, 100)
+                    begin: Alignment.bottomRight,
+                    stops: [
+                      0.2,
+                      0.9
+                    ],
+                    colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.1)
+                    ])),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Icon(
+                        Icons.directions_run,
+                        size: 40,
+                        color: Colors.black38,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        "Step Counter",
+                        style: TextStyle(
+                            color: Colors.black38,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
                   ],
                 ),
-                borderRadius: BorderRadius.circular(10)),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      stops: [
-                        0.2,
-                        0.9
-                      ],
-                      colors: [
-                        Colors.black.withOpacity(.8),
-                        Colors.black.withOpacity(.1)
-                      ])),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                SizedBox(height: 15),
+                Center(
+                  child: Column(
                     children: [
-                      Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(
-                          Icons.directions_run,
-                          size: 40,
-                          color: Colors.black38,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Text(
-                          "Step Counter",
-                          style: TextStyle(
-                              color: Colors.black38,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Text("You took", style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold),),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "0",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 50,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Column(children: [
+                                    Text(
+                                      " Steps",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 45, fontWeight: FontWeight.bold),
+                                    ),
+                                  ]),
+                                ],
+                              ),
+                              Text("today", style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold),)
+                            ],
+                          ),
+                          SizedBox(width: 20,),
+                          Image(
+                            image: AssetImage("assets/jogging.png"),
+                            width: 100,
+                            height: 100,
+                          ),
+                        ],
                       )
                     ],
                   ),
-                  SizedBox(height: 15),
-                  Center(
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Column(
-                              children: [
-                                Text("You took", style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 35,
-                                    fontWeight: FontWeight.bold),),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      "0",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 50,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Column(children: [
-                                      Text(
-                                        " Steps",
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 45, fontWeight: FontWeight.bold),
-                                      ),/*
-                            Text(
-                              "Workout",
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 20),
-                            )
-                            */
-                                    ]),
-                                  ],
-                                ),
-                                Text("today", style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 35,
-                                    fontWeight: FontWeight.bold),)
-                              ],
-                            ),
-                            SizedBox(width: 20,),
-                            Image(
-                              image: AssetImage("assets/jogging.png"),
-                              width: 100,
-                              height: 100,
-                            ),
-                          ],
-                        )
-
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ),
